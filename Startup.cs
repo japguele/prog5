24034d8633ecae9e0eba9af31d723a6b6f5de72f
japguele <japguele@student.avans.nl>
2015-12-09 13:38:30 +0100

    a

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(prog5.Startup))]
+namespace prog5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
